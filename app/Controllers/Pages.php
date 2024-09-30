f9fda0afce736ab4b0655c908a6cd7af97c1fb16
Shreejai Raj <shreejairaj@gmail.com>
2024-09-30 12:58:16 +1000

    home and about routing

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -12,7 +12,7 @@ class Pages extends BaseController
 
     public function view(string $page = 'home')
     {
-        if(! is_file(APPPATH . '/Views/pages/' . $page . 'php')) {
+        if(! is_file(APPPATH . '/Views/pages/' . $page . '.php')) {
           // Whoops, we don't have a page for that!
           throw new PageNotFoundException($page);
         }
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html>
-  <head>CodeIgniter</head>
+  <head>
+  <script src="https://cdn.tailwindcss.com"></script>
+
+  </head>
+  
   <body>
     <h1><?= esc($title) ?></h1>
   
